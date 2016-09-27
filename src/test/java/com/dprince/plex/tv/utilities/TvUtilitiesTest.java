7e50484542648cabf1f125dab0d1f94468edb0d7
Darren Prince <darprince@hotmail.com>
2016-09-26 17:11:58 -0700

    added asserts into test

--- src/test/java/com/dprince/plex/tv/utilities/TvUtilitiesTest.java
+++ src/test/java/com/dprince/plex/tv/utilities/TvUtilitiesTest.java
@@ -1,6 +1,10 @@
 package com.dprince.plex.tv.utilities;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import com.dprince.plex.tv.types.TvShow;
 
 public class TvUtilitiesTest {
     private static final String[] RAW_FILENAMES = {
@@ -9,14 +13,19 @@ public class TvUtilitiesTest {
             "Outcast.thing.1x09.WEB-DL.XviD-FUM[ettv].avi",
             "Outcast.thing.0109.WEB-DL.1080p.XviD-FUM[ettv].mpg",
             "Outcast.thing.1of9.WEB-DL.XviD-FUM[ettv].mpg",
-            "animal (2016).1of9.WEB-DL.XviD-FUM[ettv].mpg",
+            "outcast.thing (2016).1of9.WEB-DL.XviD-FUM[ettv].mpg",
             "Outcast.thing.part.9.WEB-DL.XviD-FUM[ettv].mpg"
     };
 
     @Test
     public void parseFileNameTest() {
         for (final String filename : RAW_FILENAMES) {
-            TvUtilities.parseFileName(filename);
+            final TvShow tvShow = TvUtilities.parseFileName(filename);
+            assertEquals("Tv Raw Showname ", tvShow.getRawTvShowName(), "Outcast Thing");
+            assertEquals("Episode Number ", tvShow.getTvEpisodeNumber(), "09");
+            assertEquals("Episode Season ", tvShow.getTvSeasonNumber(), "01");
+            System.out.println(filename + " passed.");
+            System.out.println(tvShow.getYear());
         }
     }
 
