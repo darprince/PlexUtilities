9c5321bd2b1a51bd61102207bb08d96ef313d58a
Darren Prince <darprince@hotmail.com>
2016-10-19 13:01:17 -0700

    Added Marvels agents of shield to AkwardTvShows

--- src/main/java/com/dprince/plex/tv/utilities/AwkwardTvShows.java
+++ src/main/java/com/dprince/plex/tv/utilities/AwkwardTvShows.java
@@ -1,7 +1,8 @@
 package com.dprince.plex.tv.utilities;
 
 public enum AwkwardTvShows {
-    TOSH("tosh 0", "Tosh.0");
+    TOSH("tosh 0", "Tosh.0"),
+    AGENTS_OF_SHIELD("marvels agents of s h i e l d", "Marvel's, Agents of S.H.I.E.L.D");
 
     String match;
     String replacement;
