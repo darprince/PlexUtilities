3e71a0713fb7c3d60716a1efa2c6fbe07bd8b7de
Darren Prince <darprince@hotmail.com>
2016-10-24 10:14:11 -0700

    Added episode check

---
+++ src/main/java/episodecheck/EpisodeCheck.java
@@ -0,0 +1,27 @@
+package episodecheck;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+
+import com.dprince.logger.Logging;
+
+public class EpisodeCheck {
+    public static final String DESKTOP_SHARED_DIRECTORIES[] = {
+            "tv a-e", "tv f-l", "tv m-s", "tv t-z", "Kids Tv"
+    };
+    private static final String PLEX_PREFIX = "\\\\Desktop-plex\\";
+
+    private static final Logger LOG = Logging.getLogger(EpisodeCheck.class);
+
+    public static void checkEpisodes() {
+        for (final String drive : DESKTOP_SHARED_DIRECTORIES) {
+            final File driveLocation = new File(PLEX_PREFIX + drive);
+            final File[] folderList = driveLocation.listFiles();
+
+            for (final File folder : folderList) {
+
+            }
+        }
+    }
+}
